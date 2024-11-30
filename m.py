71ad3175eeb4b1ff5415f0c8a078c490f1aebf81
vishwajeetsuhaspatil <bhagyashreesushv2@gmail.com>
2024-11-30 20:39:38 +0530

    mm2

--- m.py
+++ m.py
@@ -2,7 +2,7 @@ from math import sqrt
 number= int(input("enter a number"))
 if number > 1:
     for k in range(2,int(sqrt(number))+ 1):
-     if(number % i == 0):
+     if(number % k == 0):
         print("its a prime number")
         break
     else:
